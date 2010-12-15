1011617905aaf0859d918cac8384fcd5d8af5b12
le mentec fabien <fabien.lementec@imag.fr>
2010-12-15 21:52:38 +0100

    [minor]

--- run/run.sh
+++ run/run.sh
@@ -7,11 +7,12 @@ COUNT=100000
 DEGREE=40
 GRAPH=../dat/$COUNT\_$DEGREE.dat ;
 FROM=0
-TO=501
+TO=5001
 
-taskset -c 0 ../build/bfs_seq $GRAPH $FROM $TO ;
+#taskset -c 0 ../build/bfs_seq $GRAPH $FROM $TO ;
 
-for i in `seq 0 $((PROC_MAX - 1))`; do
+#for i in `seq 0 $((PROC_MAX - 1))`; do
+for i in `seq 10 $((PROC_MAX - 1))`; do
     LD_LIBRARY_PATH=$XKAAPIDIR/lib \
     KAAPI_CPUSET=0:$i \
     ../build/bfs_par $GRAPH $FROM $TO ;
